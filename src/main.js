4b3b9908ff99fd5fc0548009a604ac6b31af25fe
ziippy <ziippy@naver.com>
2022-07-26 19:52:15 +0900

    vuex 설치 및 의존성 및 기본 구조 추가

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios'
+import store from './store'
 
 Vue.prototype.$axios = axios;
 //createApp(App).mount('#app')
 new Vue({
+  store,
   render: h => h(App)
 }).$mount('#app')
 
